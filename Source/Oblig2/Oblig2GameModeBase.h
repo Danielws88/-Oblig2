2b39135adf6f60c4689ac9a55d00213cc901ca16
Danielws88 <111863611+Danielws88@users.noreply.github.com>
2023-03-04 17:08:42 +0100

    Oblig2

---
+++ Source/Oblig2/Oblig2GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Oblig2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OBLIG2_API AOblig2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
